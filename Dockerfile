7972c4cad780fc3367b7ff08a9a6d7a6c4b89e68
Iliass Tiendrebeogo <iliasstiendre@Iliasss-MacBook-Pro.local>
2023-02-21 10:32:29 -0800

    setup containers services

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3
+ENV PYTHONUNBUFFERED=1
+WORKDIR /app
+COPY requirements.txt requirements.txt
+RUN pip install --upgrade pip
+RUN pip install -r requirements.txt
+COPY . /app 
+EXPOSE 8000 
+EXPOSE 8001 
---
+++ main.py
@@ -0,0 +1,21 @@
+import strawberry
+
+from fastapi import FastAPI
+from strawberry.fastapi import GraphQLRouter
+
+app = FastAPI()
+
+@strawberry.type
+class Query:
+    @strawberry.field
+    def hello(self) -> str:
+        return "Hello World"
+
+
+schema = strawberry.Schema(Query)
+
+graphql_app = GraphQLRouter(schema)
+
+app = FastAPI()
+app.include_router(graphql_app, prefix="/graphql")
+
