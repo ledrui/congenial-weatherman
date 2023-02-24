a8480809f0e2dc69c6b5f4540d8f6734ccf8cccf
Iliass Tiendrebeogo <iliasstiendre@Iliasss-MacBook-Pro.local>
2023-02-24 00:19:27 -0800

    add schemas

--- schemas.py
+++ schemas.py
@@ -8,7 +8,7 @@ from models import User as UserModel
 @strawberry.experimental.pydantic.type(model=WeatherDataModel, 
                                        fields=["date_time", "temperature", "humidity"])
 class WeatherData:
-    date_time = int
+    date_time = str
     temperature = float
     humidity = float
 
@@ -19,14 +19,18 @@ class CityDataType:
 
 @strawberry.experimental.pydantic.type(model=UserModel, fields=["first_name", "last_name"])
 class User:
-   id = strawberry.ID
    first_name = str
    last_name = str
-   favorite = Optional [List[CityDataType]]
+
+   favorites = Optional[List[CityDataType]]
+
+   @strawberry.field
+   async def favorites(self, info) -> List[CityDataType]:
+       return [CityDataType(name=city.name) for city in self.favorites]
 
 @strawberry.input
 class WeatherDataInput:
-    date_time: int
+    date_time: str
     temperature: Optional[float] = strawberry.UNSET
     humidity: Optional[float] = strawberry.UNSET
 
@@ -35,8 +39,21 @@ class UserNotFound:
     message: str = "Couldn't find an user with the given name"
 
 @strawberry.type
+class UserAlreadyExists:
+    message: str = "This user already exists"
+
+@strawberry.type
 class UserHasNoFavorites:
     message: str = "This user has no favorites"
 
+@strawberry.type
+class IDType:
+    id: strawberry.ID
 
+
+@strawberry.type
+class UserResponse:
+    id: strawberry.ID
+    first_name: str
+    last_name: str
 
